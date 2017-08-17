c2b1d19850e3f13d9b7951417dca531ebb13a45d
Zach Hawkes <hawkeye.zach@gmail.com>
2017-08-17 13:50:23 -0600

    Adding validation for attrecord

--- app/models/attrecord.rb
+++ app/models/attrecord.rb
@@ -1,3 +1,7 @@
 class Attrecord < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :PTO, numericality: {greater_than_or_equal_to: 0}
+  validates :FMLA, numericality: {greater_than_or_equal_to: 0}
+  validates :days, numericality: {greater_than_or_equal_to: 0, less_than: 4}
 end
