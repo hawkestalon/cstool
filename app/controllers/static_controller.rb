c0b2ded2a195f25476fb20fe9be921167151a917
Zach Hawkes <hawkeye.zach@gmail.com>
2017-08-24 12:31:43 -0600

    Adding announcements

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,5 @@
 class StaticController < ApplicationController
   def home
+    @announcements = Announcement.all
   end
 end
--- app/models/attrecord.rb
+++ app/models/attrecord.rb
@@ -1,7 +1,3 @@
 class Attrecord < ApplicationRecord
-  belongs_to :user
-  validates :user_id, presence: true
-  validates :PTO, numericality: {greater_than_or_equal_to: 0}
-  validates :FMLA, numericality: {greater_than_or_equal_to: 0}
-  validates :days, numericality: {greater_than_or_equal_to: 0, less_than: 4}
+    belongs_to :user
 end
